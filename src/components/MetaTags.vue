a7e13f02d27243551dcf4e7bf3c7250184cc2aed
Akayy <techakayy@gmail.com>
2023-09-25 15:28:04 +1000

    Meta updates

--- src/components/MetaTags.vue
+++ src/components/MetaTags.vue
@@ -5,6 +5,7 @@
 
   import site from '@/site'
   import image from '@/screenshots/image.jpg'
+  // import { pg_font_urls } from '~~/themes/pg-tailwindcss/tokens.mjs'
 
   const link: any = [
     {
@@ -14,6 +15,26 @@
     },
   ]
   const noscript: any = []
+
+  // if (pg_font_urls.length) {
+  //   const googleapis = 'https://fonts.googleapis.com'
+  //   const gstatic = 'https://fonts.gstatic.com'
+  //   link.push(
+  //     { rel: 'dns-prefetch', href: googleapis },
+  //     { rel: 'dns-prefetch', href: gstatic },
+  //     { rel: 'preconnect', crossorigin: 'anonymous', href: googleapis },
+  //     { rel: 'preconnect', crossorigin: 'anonymous', href: gstatic },
+  //     {
+  //       rel: 'preload',
+  //       as: 'style',
+  //       onload: "this.onload=null;this.rel='stylesheet'",
+  //       href: pg_font_urls.toString(),
+  //     },
+  //   )
+  //   noscript.push(
+  //     `<link rel="stylesheet" crossorigin="anonymous" href="${pg_font_urls.toString()}" />`,
+  //   )
+  // }
 
   const { title, description, url } = site
   const { meta, frontmatter } = usePage()
