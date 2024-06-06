d1149d965d213ffa162b6b6216b3dd87973f0e26
Akayy <techakayy@gmail.com>
2024-06-06 17:55:00 +1000

    Initial commit

---
+++ src/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="layout">
+    <header>
+      <HeadAndMeta />
+    </header>
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+<style>
+  @import '~/assets/css/base.css';
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,19 @@
+<page>
+    title: Home
+</page>
+<template>
+  <div>
+    <div
+      style="
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        padding-top: 1.5rem /* 24px */;
+        padding-bottom: 1.5rem /* 24px */;
+      "
+    >
+      <span>A Fresh Start!</span>
+    </div>
+  </div>
+</template>
